a83581c150a723d45ceef50127600355fad61e00
Sayfullah <sayfullah.jumoorty@live.com>
2023-08-12 10:40:15 +0200

    Added directory feature

--- src/witsshell.c
+++ src/witsshell.c
@@ -12,18 +12,24 @@
 #define MAX_ARGUMENTS 100
 #define MAX_PATH_LENGTH 1024
 
-char path[MAX_PATH_LENGTH] = "/bin"; // Initial search path
+char searchPath[MAX_PATH_LENGTH] = "/bin"; // Initial search path
 
-// Function to parse and execute the CD command
-void execute_cd(char *args[])
+// Function to display error message
+void displayError(const char *message)
+{
+	write(STDERR_FILENO, message, strlen(message));
+}
+
+// Function to execute the CD command
+void executeCD(char *args[])
 {
 	if (args[1] == NULL)
 	{
-		write(STDERR_FILENO, "An error has occurred\n", 23);
+		displayError("An error has occurred\n");
 	}
 	else if (args[2] != NULL)
 	{
-		write(STDERR_FILENO, "An error has occurred\n", 23);
+		displayError("An error has occurred\n");
 	}
 	else
 	{
@@ -35,15 +41,72 @@ void execute_cd(char *args[])
 }
 
 // Function to set the search path
-void set_path(char *args[])
+void setPath(char *args[])
 {
 	if (args[1] == NULL)
 	{
-		write(STDERR_FILENO, "An error has occurred\n", 23);
+		displayError("An error has occurred\n");
 	}
 	else
 	{
-		strcpy(path, args[1]);
+		strcpy(searchPath, args[1]);
+	}
+}
+
+// Function to check for built-in commands
+bool isBuiltInCommand(const char *command)
+{
+	return strcmp(command, "exit") == 0 || strcmp(command, "cd") == 0 ||
+		   strcmp(command, "path") == 0 || strcmp(command, "clear") == 0;
+}
+
+// Function to redirect output to a file
+void redirectOutput(const char *outputFile)
+{
+	int fd = open(outputFile, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	if (fd == -1)
+	{
+		perror("open");
+		exit(1);
+	}
+	dup2(fd, STDOUT_FILENO);
+	dup2(fd, STDERR_FILENO);
+	close(fd);
+}
+
+// Function to execute a command
+void executeCommand(char *args[], bool parallel, const char *outputFile)
+{
+	pid_t childPid = fork();
+	if (childPid == -1)
+	{
+		perror("fork");
+		exit(1);
+	}
+	else if (childPid == 0)
+	{
+		// Child process
+		if (parallel)
+		{
+			setpgid(0, 0); // Set new process group for parallel execution
+		}
+
+		if (outputFile)
+		{
+			redirectOutput(outputFile);
+		}
+
+		char fullPath[MAX_PATH_LENGTH + strlen(args[0]) + 2];
+		snprintf(fullPath, sizeof(fullPath), "%s/%s", searchPath, args[0]);
+		execv(fullPath, args);
+		perror("execv");
+		exit(1);
+	}
+	else if (!parallel)
+	{
+		// Parent process for sequential execution
+		int status;
+		waitpid(childPid, &status, 0);
 	}
 }
 
@@ -52,7 +115,7 @@ int main(int argc, char *argv[])
 	bool batchMode = false;
 	FILE *inputStream = stdin;
 
-	// Check if running in batch mode
+	// Check for batch mode
 	if (argc == 2)
 	{
 		batchMode = true;
@@ -65,33 +128,31 @@ int main(int argc, char *argv[])
 	}
 	else if (argc > 2)
 	{
-		write(STDERR_FILENO, "An error has occurred\n", 23);
+		displayError("An error has occurred\n");
 		exit(1);
 	}
 
 	char input[MAX_INPUT_LENGTH];
 	char *args[MAX_ARGUMENTS];
-	bool redirectOutput = false;
-	char *outputFile = NULL;
 
 	while (1)
 	{
+		char currentDirectory[MAX_PATH_LENGTH];
+		getcwd(currentDirectory, sizeof(currentDirectory));
 		if (!batchMode)
 		{
-			printf("witsshell> ");
+			printf("witsshell (%s)> ", currentDirectory);
 			fflush(stdout);
 		}
 
-		// Read input from the appropriate stream
+		// Read input
 		if (fgets(input, sizeof(input), inputStream) == NULL)
 		{
 			break; // End of file or error
 		}
 
-		// Remove trailing newline character
-		input[strlen(input) - 1] = '\0';
+		input[strlen(input) - 1] = '\0'; // Remove trailing newline
 
-		// Parse the input into arguments
 		int argCount = 0;
 		args[argCount] = strtok(input, " ");
 		while (args[argCount] != NULL)
@@ -103,137 +164,62 @@ int main(int argc, char *argv[])
 
 		if (argCount > 0)
 		{
-			// Check for built-in commands
-			if (strcmp(args[0], "exit") == 0)
+			if (isBuiltInCommand(args[0]))
 			{
-				if (argCount > 1)
+				if (strcmp(args[0], "exit") == 0)
 				{
-					write(STDERR_FILENO, "An error has occurred\n", 23);
-				}
-				else
-				{
-					exit(0); // Exit the shell
-				}
-			}
-			else if (strcmp(args[0], "cd") == 0)
-			{
-				execute_cd(args); // Execute CD command
-				continue;		  // Continue to the next iteration of the loop
-			}
-			else if (strcmp(args[0], "path") == 0)
-			{
-				set_path(args); // Set the search path
-				continue;		// Continue to the next iteration of the loop
-			}
-			else if (strcmp(args[0], "clear") == 0)
-			{
-				system("clear"); // Clear the screen
-				continue;		 // Continue to the next iteration of the loop
-			}
-		}
-
-		// Check for output redirection
-		redirectOutput = false;
-		for (int i = argCount - 1; i > 0; i--)
-		{
-			if (strcmp(args[i], ">") == 0)
-			{
-				if (i + 1 < argCount)
-				{
-					redirectOutput = true;
-					args[i] = NULL;			  // Remove the ">" argument
-					outputFile = args[i + 1]; // Store the output file
-					break;
-				}
-				else
-				{
-					write(STDERR_FILENO, "An error has occurred\n", 23);
-					continue;
-				}
-			}
-		}
-
-		// Check for parallel execution
-		bool parallel = false;
-		for (int i = 0; i < argCount; i++)
-		{
-			if (strcmp(args[i], "&") == 0)
-			{
-				parallel = true;
-				args[i] = NULL; // Remove the "&" argument
-				break;
-			}
-		}
-
-		if (parallel)
-		{
-			// Fork a child process for parallel execution
-			pid_t child_pid = fork();
-			if (child_pid == -1)
-			{
-				perror("fork");
-				exit(1);
-			}
-			else if (child_pid == 0)
-			{
-				// Child process
-				if (redirectOutput)
-				{
-					int fd = open(outputFile, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-					if (fd == -1)
+					if (argCount > 1)
 					{
-						perror("open");
-						exit(1);
+						displayError("An error has occurred\n");
 					}
-					dup2(fd, STDOUT_FILENO);
-					dup2(fd, STDERR_FILENO);
-					close(fd);
-				}
-
-				char full_path[MAX_PATH_LENGTH + strlen(args[0]) + 2];
-				snprintf(full_path, sizeof(full_path), "%s/%s", path, args[0]);
-				execv(full_path, args);
-				perror("execv");
-				exit(1);
-			}
-		}
-		else
-		{
-			// Fork a child process for sequential execution
-			pid_t child_pid = fork();
-			if (child_pid == -1)
-			{
-				perror("fork");
-				exit(1);
-			}
-			else if (child_pid == 0)
-			{
-				// Child process
-				if (redirectOutput)
-				{
-					int fd = open(outputFile, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-					if (fd == -1)
+					else
 					{
-						perror("open");
-						exit(1);
+						exit(0); // Exit the shell
 					}
-					dup2(fd, STDOUT_FILENO);
-					dup2(fd, STDERR_FILENO);
-					close(fd);
+				}
+				else if (strcmp(args[0], "cd") == 0)
+				{
+					executeCD(args);
+				}
+				else if (strcmp(args[0], "path") == 0)
+				{
+					setPath(args);
+				}
+				else if (strcmp(args[0], "clear") == 0)
+				{
+					system("clear");
+				}
+				continue;
+			}
+
+			char *commandArgs[MAX_ARGUMENTS];
+			int commandArgCount = 0;
+			bool parallel = false;
+			const char *outputFile = NULL;
+
+			for (int i = 0; i < argCount; i++)
+			{
+				if (strcmp(args[i], ">") == 0)
+				{
+					if (i + 1 < argCount)
+					{
+						outputFile = args[i + 1]; // Store the output file
+						break;
+					}
+					else
+					{
+						displayError("An error has occurred\n");
+						continue;
+					}
 				}
 
-				char full_path[MAX_PATH_LENGTH + strlen(args[0]) + 2];
-				snprintf(full_path, sizeof(full_path), "%s/%s", path, args[0]);
-				execv(full_path, args);
-				perror("execv");
-				exit(1);
+				commandArgs[commandArgCount] = args[i];
+				commandArgCount++;
 			}
-			else
-			{
-				// Parent process
-				int status;
-				waitpid(child_pid, &status, 0);
-			}
+
+			commandArgs[commandArgCount] = NULL; // Null-terminate the command argument list
+
+			executeCommand(commandArgs, parallel, outputFile);
 		}
 	}
 
