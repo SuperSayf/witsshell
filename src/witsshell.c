7768c32f2a6c4a42e25224d7a3876d58d056cdca
Sayfullah <sayfullah.jumoorty@live.com>
2023-09-05 21:23:51 +0200

    save

--- src/witsshell.c
+++ src/witsshell.c
@@ -11,8 +11,9 @@
 #define MAX_INPUT_LENGTH 1024
 #define MAX_ARGUMENTS 100
 #define MAX_PATH_LENGTH 1024
+#define MAX_PATHS 100
 
-char searchPath[MAX_PATH_LENGTH] = "/bin"; // Initial search path
+char searchPaths[MAX_PATHS][MAX_PATH_LENGTH] = {"/bin"}; // Initial search paths
 
 // Function to display error message
 void displayError(const char *message)
@@ -43,15 +44,28 @@ void executeCD(char *args[])
 // Function to set the search path
 void setPath(char *args[])
 {
-    if (args[1] == NULL)
+    int pathCount = 0;
+
+    for (int i = 1; args[i] != NULL; i++)
     {
-        // Empty path
-        strcpy(searchPath, "");
+        char *path = strtok(args[i], " ");
+        while (path != NULL)
+        {
+            if (pathCount == MAX_PATHS)
+            {
+                displayError("An error has occurred\n");
+                return;
+            }
+
+            strcpy(searchPaths[pathCount], path);
+            pathCount++;
+
+            path = strtok(NULL, " ");
+        }
     }
-    else
-    {
-        strcpy(searchPath, args[1]);
-    }
+
+    // Null-terminate the search path
+    searchPaths[pathCount][0] = '\0';
 }
 
 // Function to check for built-in commands
@@ -125,8 +139,31 @@ void executeCommand(char *args[], const char *outputFile)
     else if (childPid == 0)
     {
         // Child process
-        char fullPath[MAX_PATH_LENGTH + strlen(args[0]) + 2];
-        snprintf(fullPath, sizeof(fullPath), "%s/%s", searchPath, args[0]);
+        char fullPath[MAX_PATH_LENGTH];
+        bool found = false;
+
+        // Iterate through the search paths
+        for (int i = 0; searchPaths[i][0] != '\0'; i++)
+        {
+            strcpy(fullPath, searchPaths[i]);
+            strcat(fullPath, "/");
+            strcat(fullPath, args[0]);
+
+            // Check if the command exists in the current search path
+            if (access(fullPath, X_OK) == 0)
+            {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found)
+        {
+            displayError("An error has occurred\n");
+            exit(1);
+        }
+
+        // Execute the command
         execv(fullPath, args);
         displayError("An error has occurred\n");
         exit(1);
@@ -143,41 +180,53 @@ void executeCommand(char *args[], const char *outputFile)
     close(originalStdout);
 }
 
-void executeCommandsInParallel(char *parallelCommands[], const char *outputFile)
+// Function to execute commands in parallel
+void executeCommandsInParallel(char *parallelCommands[], char *args[], const char *outputFile)
 {
-    int originalStdout = dup(STDOUT_FILENO); // Save the original STDOUT
+    // Array to hold child process IDs
+    pid_t childPids[MAX_ARGUMENTS];
 
-    if (outputFile)
+    // Initialize an index for child processes
+    int childCount = 0;
+
+    // Iterate through parallel commands
+    for (int i = 0; parallelCommands[i] != NULL; i++)
     {
-        // Redirect output to the specified file
-        redirectOutput(outputFile);
+        int argCount = 0;
+        args[argCount] = strtok(parallelCommands[i], " ");
+        while (args[argCount] != NULL)
+        {
+            argCount++;
+            args[argCount] = strtok(NULL, " ");
+        }
+        args[argCount] = NULL; // Null-terminate the argument list
+
+        // Create a child process
+        pid_t childPid = fork();
+        if (childPid == -1)
+        {
+            displayError("An error has occurred\n");
+            exit(1);
+        }
+        else if (childPid == 0)
+        {
+            // Child process
+            executeCommand(args, outputFile);
+            exit(0);
+        }
+        else
+        {
+            // Parent process
+            childPids[childCount++] = childPid;
+        }
     }
 
-    pid_t childPid = fork();
-    if (childPid == -1)
+    // Wait for all child processes to complete
+    for (int i = 0; i < childCount; i++)
     {
-        displayError("An error has occurred\n");
-        exit(1);
-    }
-    else if (childPid == 0)
-    {
-        // Child process
-        char fullPath[MAX_PATH_LENGTH + strlen(parallelCommands[0]) + 2];
-        snprintf(fullPath, sizeof(fullPath), "%s/%s", searchPath, parallelCommands[0]);
-        execv(fullPath, parallelCommands);
-        displayError("An error has occurred\n");
-        exit(1);
-    }
-    else
-    {
-        // Parent process for parallel execution
         int status;
-        waitpid(childPid, &status, 0);
+        waitpid(childPids[i], &status, 0);
     }
-
-    // Restore the original STDOUT and close the duplicated file descriptor
-    dup2(originalStdout, STDOUT_FILENO);
-    close(originalStdout);
 }
 
 int main(int argc, char *argv[])
@@ -340,82 +389,10 @@ int main(int argc, char *argv[])
             {
                 executeCommand(commandArgs, outputFile);
             }
-            else
+            else if (shouldRun && parallel)
             {
                 // Execute the parallel commands
-                for (int i = 0; i < parallelCommandCount; i++)
-                {
-                    int argCount = 0;
-                    args[argCount] = strtok(parallelCommands[i], " ");
-                    while (args[argCount] != NULL)
-                    {
-                        argCount++;
-                        args[argCount] = strtok(NULL, " ");
-                    }
-                    args[argCount] = NULL; // Null-terminate the argument list
-
-                    if (argCount > 0)
-                    {
-                        char *commandArgs[MAX_ARGUMENTS];
-                        int commandArgCount = 0;
-                        const char *outputFile = NULL;
-                        bool shouldRun = true;
-                        bool outputDetected = false; // Track if an output file has been detected
-
-                        for (int i = 0; i < argCount; i++)
-                        {
-                            // Check for output redirection
-                            if (strcmp(args[i], ">") == 0)
-                            {
-                                if (i == 0 || i == argCount - 1)
-                                {
-                                    displayError("An error has occurred\n");
-                                    shouldRun = false;
-                                    break;
-                                }
-                                else
-                                {
-                                    if (outputDetected)
-                                    {
-                                        displayError("An error has occurred\n");
-                                        shouldRun = false;
-                                        break;
-                                    }
-                                    outputFile = args[i + 1]; // Store the output file
-                                    outputDetected = true;
-                                    i++; // Skip the next argument (the output file)
-                                    continue;
-                                }
-                            }
-
-                            // Check if the argument contains ">" without being an output file
-                            if (strstr(args[i], ">") != NULL)
-                            {
-                                displayError("An error has occurred\n");
-                                shouldRun = false;
-                                break;
-                            }
-
-                            if (outputDetected)
-                            {
-                                // If an output file has been detected, this argument is an additional output file
-                                displayError("An error has occurred\n");
-                                shouldRun = false;
-                                break;
-                            }
-
-                            commandArgs[commandArgCount] = args[i];
-                            commandArgCount++;
-                        }
-
-                        commandArgs[commandArgCount] = NULL; // Null-terminate the command argument list
-
-                        if (shouldRun)
-                        {
-                            executeCommandsInParallel(commandArgs, outputFile);
-                        }
-                    }
-                }
+                executeCommandsInParallel(parallelCommands, commandArgs, outputFile);
             }
         }
     }
